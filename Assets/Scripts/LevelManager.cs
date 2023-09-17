24ffed982f37be674b4849a506fa786c2124c276
Michael Artner <michael.artner@hotmail.de>
2023-09-17 10:21:00 +0200

    removed unused imports

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
